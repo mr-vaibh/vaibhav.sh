86f86c687901008f3f5b2986af9fab6ceea9a900
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-03-30 23:27:51 +0530

    setup dynamic pages and it's url

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 def index(request):
     return render(request, 'home/index.html')
+
+def path(request, path):
+    return TemplateView.as_view(template_name='home/site_content/' + path)(request)
