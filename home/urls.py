68182cb50661f2e9e5d2b0e46812969e7308d13c
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-03-31 01:31:40 +0530

    added resume page

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", TemplateView.as_view(template_name='home/index.html'), name="index"),
+    path("resume", TemplateView.as_view(template_name='home/resume.html'), name="resume"),
+    # Site content
     path("<path:path>", views.path, name="path"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,8 +3,5 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'home/index.html')
-
 def path(request, path):
     return TemplateView.as_view(template_name='home/site_content/' + path)(request)
