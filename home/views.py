ceaf0afb41ee9053993646b4770990ea6e11d063
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-04-11 16:30:01 +0530

    added 404 page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponseNotFound
+from django.conf import settings
+
+import os
 
 # Create your views here.
 
 def path(request, path):
-    return TemplateView.as_view(template_name='home/site_content/' + path)(request)
+    template_path = 'home/site_content/' + path
+    template_exist = os.path.normpath(settings.BASE_DIR / 'home/templates/' / template_path)
+
+    if os.path.exists(template_exist):
+        return TemplateView.as_view(template_name=template_path)(request)
+    else:
+        return HttpResponseNotFound(TemplateView.as_view(template_name='home/404.html')(request).render())
