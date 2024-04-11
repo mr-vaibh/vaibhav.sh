568bf906317d08a4e005908733337ff436373fc1
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-04-11 16:30:32 +0530

    added tales, cropper page

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,8 @@ app_name = 'home'
 urlpatterns = [
     path("", TemplateView.as_view(template_name='home/index.html'), name="index"),
     path("resume", TemplateView.as_view(template_name='home/resume.html'), name="resume"),
+    path("tales", TemplateView.as_view(template_name='home/tales.html'), name="tales"),
+    path("cropper", TemplateView.as_view(template_name='home/cropper.html'), name="cropper"),
     # Site content
     path("<path:path>", views.path, name="path"),
 ]
